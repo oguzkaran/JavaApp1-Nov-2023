beeef58c094b799d3c23d8166d618cb71e8d1c5f
Oğuz Karan <oguzkaran@csystem.org>
2023-11-12 13:29:21 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,9 +3,18 @@
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import java.util.Scanner;
+
 class Application {
     public static void run(String[] args)
     {
-        System.out.println("Hello, World");
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("İki sayı giriniz:");
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+
+        System.out.printf("%d + %d = %d%n", a, b, a + b);
+
     }
 }
