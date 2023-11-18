bde0c294c0704554ef3747ffcf01835bf00177ec
Oğuz Karan <oguzkaran@csystem.org>
2023-11-18 13:22:43 +0300

    Update Application.java

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,16 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    Genel olarak bir kütüphane maven içerisinden aşağıdaki durumlardan biri ya da birkaçından kullanılabilir:
+    - Uygulama geliştirilirken kullanılan host makeinenin içerisindeki maven loal repository'den kullanılabilir.
+    Bu repository maven programı geçerli veya geçersiz ilk kez çalıştırıldığında genel olarak .m2 isimli bir
+    directory biçiminde yartılır. Bu directory'nin aslında default ismi ve default yeri değiştirilebilir. Ancak pratikte
+    özel durumlar dışında ismi ve yeri değiştirilmez.
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import com.karandev.io.util.console.Console;
 
 class Application {
     public static void run(String[] args)
     {
-        int a = Console.readInt("Birinci sayıyı giriniz:", "Hatalı giriş yaptınız!...");
-        int b = Console.readInt("İkinci sayıyı giriniz:", "Hatalı giriş yaptınız!...");
 
-        Console.writeLine("%d + %d = %d", a, b, a + b);
     }
 }
