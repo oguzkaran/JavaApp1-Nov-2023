42943c323530c7b80a80671eefd590aea54ccad7
Oğuz Karan <oguzkaran@csystem.org>
2023-12-04 17:07:15 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,31 +1,12 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki örnekte aynı sayılar için isPrime metotlarının performanslarını gözlemleyiniz
+    -Djava.library.path=/opencv/build/lib
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import com.karandev.io.util.console.Console;
-import org.apache.commons.lang3.time.StopWatch;
-import org.csystem.util.numeric.NumberUtil;
-
-import java.math.BigInteger;
 
 class Application {
     public static void run(String[] args)
     {
-        var stopWatch = new StopWatch();
 
-        stopWatch.start();
-        Console.writeLine(NumberUtil.isPrime(710_584_055_392_819_667L));
-        stopWatch.stop();
-
-        Console.writeLine("%f seconds", stopWatch.getNanoTime() / 1_000_000_000.);
-
-        stopWatch.reset();
-        stopWatch.start();
-        Console.writeLine(NumberUtil.isPrime(BigInteger.valueOf(710_584_055_392_819_667L)));
-        stopWatch.stop();
-
-        Console.writeLine("%f seconds", stopWatch.getNanoTime() / 1_000_000_000.);
     }
 }
 
