2d18f719294361ca842a46fc9a56d48b4e9d694b
Oğuz Karan <oguzkaran@csystem.org>
2024-01-14 16:40:32 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,35 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Sınıf Çalışması: stdin'den girilen doğum tarihi bilgisine göre aşağıdaki mesajlar uygun olanı stdout'a basan basit
-    uygulamayı yazınız. Mesajlar:
-        1. Doğum günü geçmişse          -> Geçmiş doğum gününüz kutlu olsun. Yeni yaşınız:...
-        2. Doğum günü ise               -> Doğum gününüz kutlu olsun. Yeni yaşınız:...
-        3. Doğum günü henüz gelmemişse  -> Doğum gününüz şimdiden kutlu olsun. Yeni yaşınız:...
-
-    Burada yaş bilgisini istediğiniz duyarlılıkta yazdırabilirsiniz. Uygulama ayrı bir proje olarak yapılacaktır
+    Java 8 ile eklenen ChronoUnit isimli enum sınfı ile çeşitli birimlerde ölçümler yapılabilmektedir. Bunun için tipik
+    olarak between metodu kullanılabilir. Bu enum sınıfın detayları ileride ele alınacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 import com.karandev.io.util.console.Console;
 
-import java.util.GregorianCalendar;
-
-import static java.util.Calendar.*;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 class Application {
     public static void run(String[] args)
     {
-        while (true) {
-            var month = Console.readInt("Input expiry month:");
-            var year = Console.readInt("Input expiry year:");
-            var expiryDate = new GregorianCalendar(year, month - 1, 1);
+        var earthquakeDatetime = LocalDateTime.of(1999, Month.AUGUST, 17, 3, 2);
+        var now = LocalDateTime.now();
+        var hours = ChronoUnit.HOURS.between(earthquakeDatetime, now);
+        var days = ChronoUnit.DAYS.between(earthquakeDatetime, now);
+        var years = ChronoUnit.YEARS.between(earthquakeDatetime, now);
+        var totalYears = ChronoUnit.DAYS.between(earthquakeDatetime, now) / 365.;
 
-            expiryDate.set(DAY_OF_MONTH, expiryDate.getActualMaximum(DAY_OF_MONTH));
-            if (new GregorianCalendar().after(expiryDate))
-                Console.writeLine("KArtın son kullanma tarihi geçmiştir");
-            else
-                Console.writeLine("KArtın son kullanma tarihi geçmemiştir");
-        }
+        Console.writeLine(hours);
+        Console.writeLine(days);
+        Console.writeLine(years);
+        Console.writeLine(totalYears);
     }
 }
-
-
