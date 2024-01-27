a83deb65215e6c24847494c2dcefd26a4706180c
Oğuz Karan <oguzkaran@csystem.org>
2024-01-27 16:29:17 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,37 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aslında inner class içerisinde ait olduğu sınıf türünden bir referans veri elemanı bulunur ve nesne yaratılması
-    sırasında bu referansa ilgili adres yani inner class'ın yaratılmasında kullanılan ait olduğu sınıf türünden nesnenin
-    adresi geçilir. Bu durum tipik olarak inner class'ın her ctor'una +1 tane parametre ekleyerek yapılabilir. O
-    parametrenin türü ait olduğu sınıf türünden referanstır. Aşağıda A sınıf bildiriminin ve nesne yaratılmasının
-    yaklaşık karşılığı gösterilmiştir:
-    Bildirimin aşağı seviyedeki yaklaşık karşılığı:
-
-    class A {
-        //...
-        public void foo()
-        {
-            Console.writeLine("A.foo");
-        }
-
-        public static class B {
-            private final A m_a;
+    Anonim Sınıflar: Programcının bildirimde isim vermediği sınıflardır. Anomim sınıf bildiriminin genel biçimi
+    aşağıdaki gibidir:
+        new <UDT ismi>([parametreler]) {
             //...
-            public B(A a) //Default ctor'un yaklaşık karşılığı
-            {
-                m_a = a;
-            }
-
-            public void foo()
-            {
-                Console.writeLine("B.foo");
-                m_a.foo();
-            }
+        };
+    Örneğin:
+        new Sample() {
+            //...
         }
-    }
-
-    Nesne yaratılmasının aşağı seviyedeki yaklaşık karşılığı:
-    var a = new A();
-    var b = new A.B(a);
+    Burada Sample sınıfından türetilmiş olan bir sınıf bildirilmiştir ve o sınıf türünden nesne yaratılmıştır. Bu durumda
+    her anonim sınıf bildirimi ayrı bir sınıf bildirimi anlamındadır. Derleyici yine anonim sınıflar için byte code'u
+    üretirken $ karakteri kullanarak isimlendirme yapar. Aşağıdaki demo örneği çalılştırıp dinamik türleri gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -40,30 +19,19 @@ import org.csystem.util.console.Console;
 class Application {
     public static void run(String[] args)
     {
-        while (true) {
-            var a = new A();
+        Sample s1 = new Sample() {
+            //...
+        };
 
+        Sample s2 = new Sample() {
             //...
-            a.foo();
-            //...
-        }
+        };
+
+        Console.writeLine(s1.getClass().getName());
+        Console.writeLine(s2.getClass().getName());
     }
 }
 
-class A {
-    private B m_b;
+class Sample {
     //...
-    public void foo()
-    {
-        Console.writeLine("A.foo");
-        m_b = new B();
-    }
-
-    public class B {
-        //...
-        public void foo()
-        {
-            Console.writeLine("B.foo");
-        }
-    }
 }
