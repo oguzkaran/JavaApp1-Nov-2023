554dfcd617bec7e0ac5e4694dd3b29655909df73
Oğuz Karan <oguzkaran@csystem.org>
2024-03-16 16:25:30 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,13 +1,50 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki demo örnekte bir stack'in elemanları dolaşılmıştır
+    Aşağıdaki demo örneği inceleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
+import com.karandev.io.util.console.Console;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 class Application {
     public static void run(String[] args)
     {
-        var list = new LinkedList<Integer>();
+        var arrayList = new ArrayList<>();
+        var linkedList = new LinkedList<>();
+
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+        for (var i = 0; i < 1_000_000; ++i)
+            arrayList.add(i * 10);
+
+        stopWatch.stop();
+
+        Console.writeLine("Time of add operation in nanoseconds of ArrayList<E> is %d", stopWatch.getNanoTime());
+
+        stopWatch = new StopWatch();
+        stopWatch.start();
+        for (var i = 0; i < 1_000_000; ++i)
+            linkedList.add(i * 10);
+
+        stopWatch.stop();
+
+        Console.writeLine("Time of add operation in nanoseconds of LinkedList<E> is %d", stopWatch.getNanoTime());
+
+        stopWatch = new StopWatch();
+        stopWatch.start();
+        arrayList.get(900_000);
+        stopWatch.stop();
+
+        Console.writeLine("Time of get operation in nanoseconds of ArrayList<E> is %d", stopWatch.getNanoTime());
+
+        stopWatch = new StopWatch();
+        stopWatch.start();
+        linkedList.get(900_000);
+        stopWatch.stop();
+
+        Console.writeLine("Time of get operation in nanoseconds of LinkedList<E> is %d", stopWatch.getNanoTime());
     }
 }
