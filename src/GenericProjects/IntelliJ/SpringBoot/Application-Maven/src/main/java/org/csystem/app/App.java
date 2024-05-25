2dcff108e419433f0aa26113d738b6827af3a289
Oğuz Karan <oguzkaran@csystem.org>
2024-05-25 15:13:57 +0300

    Update

---
+++ src/GenericProjects/IntelliJ/SpringBoot/Application-Maven/src/main/java/org/csystem/app/App.java
@@ -0,0 +1,13 @@
+package org.csystem.app;
+
+import com.karandev.io.util.console.Console;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(App.class, args);
+	}
+}
