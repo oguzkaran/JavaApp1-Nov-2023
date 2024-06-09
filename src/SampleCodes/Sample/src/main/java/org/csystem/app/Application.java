4fc97469182aee95a2215307d2fda4591644a326
Oğuz Karan <oguzkaran@csystem.org>
2024-06-09 14:32:03 +0300

    Update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,11 +1,26 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Aşağıdaki demo örneği inceleyiniz
+    Aşağıdaki örnekte, int -> int.. dönüşümü int -> long... dönüşümünden daha kaliteli olduğundan long parametreli foo
+    çağrılır
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import com.karandev.io.util.console.Console;
 
 class Application {
     public static void run(String[] args)
     {
+        Sample.foo(10);
+    }
+}
 
+class Sample {
+    public static void foo(long...a)
+    {
+        Console.writeLine("foo, long...");
+    }
+
+    public static void foo(int...a)
+    {
+        Console.writeLine("foo, int...");
     }
 }
