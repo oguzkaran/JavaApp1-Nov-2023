cb010b3c6c9123aa7228a7e1c41a6517a962b345
Oğuz Karan <oguzkaran@csystem.org>
2024-08-12 22:03:30 +0300

    Update

--- src/Projects/012-SpringBoot/009-EarthquakeServiceGeonames/GeonamesEarthquakeServiceLib/target/generated-sources/annotations/org/csystem/app/service/earthquake/geonames/mapper/GeonamesMapperImpl.java
+++ src/Projects/012-SpringBoot/009-EarthquakeServiceGeonames/GeonamesEarthquakeServiceLib/target/generated-sources/annotations/org/csystem/app/service/earthquake/geonames/mapper/GeonamesMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-11T15:39:38+0300",
+    date = "2024-08-12T21:20:44+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
